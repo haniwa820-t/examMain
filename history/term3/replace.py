23a61c46f1fc4b44eb09a04841cd489106d290e2
haniwa820 <213259578+haniwa820-t@users.noreply.github.com>
2025-10-28 12:06:13 +0900

    歴史追加

---
+++ history/term3/replace.py
@@ -0,0 +1,31 @@
+import re
+
+def text_replace_asterisk(text,replacement):
+	patern = r'<strong>(.*?)</strong>'
+	if callable(replacement):
+		return re.sub(patern, replacement, text)
+	else:
+		return re.sub(patern, replacement, text)
+
+def line_replace_asterisk(lines,replacement):
+	result = []
+	for x in lines:
+		result.append(text_replace_asterisk(x,replacement))
+	return result
+
+def file_replace_asterisk(file_path,replacement):
+	with open(file_path,'r',encoding='utf-8') as f:
+		lines = f.readlines()
+	
+	new_lines = line_replace_asterisk(lines,replacement)
+	
+	with open(file_path,'w',encoding='utf-8') as f:
+		f.writelines(new_lines)
+	return new_lines
+
+if __name__ == '__main__':
+	for i in range (1,4):
+		file_name = 'print' + str(i) + '.html'
+		file_replace_asterisk(file_name,r'<span class="toggle-text" data-original-text="\1" onclick="toggleText(this)"></span>')
+	#rはraw textを意味する、エスケープシーケンスを無効にする
+	#\1は正規表現のキャプチャグループを指す、一番目の括弧で囲まれた部分を参照する
